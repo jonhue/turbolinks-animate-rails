87d30539a65bbb159c6bbc4ea1baa354059020a6
Jonas Hübotter <me@jonhue.me>
2018-08-14 18:45:26 +0200

    [#8] Implement CI

--- lib/turbolinks-animate.rb
+++ lib/turbolinks-animate.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 require 'turbolinks-animate/version'
 
 module TurbolinksAnimate
-
-    require 'turbolinks-animate/engine'
-    require 'turbolinks-animate/railtie'
-
+  require 'turbolinks-animate/engine'
+  require 'turbolinks-animate/railtie'
 end
